a39ca00e950b4b7f4ddc9548d391e6385e7e3c27
fatfisz <fatfisz@gmail.com>
2016-04-07 23:47:48 +0200

    Fix output.

--- src/index.js
+++ src/index.js
@@ -5,7 +5,13 @@ export default (options = {}) => ({
   transform(grammar, id) {
     const { target = "es6", include = ["*.pegjs", "**/*.pegjs"], exclude } = options;
     const filter = createFilter(include, exclude);
-    const exporter = target == "es6" ? "export default" : "module.exports =";
-    return filter(id) ? { code: `${exporter} ${buildParser(grammar, { output: "source" })};`, map: { mappings: "" } } : null;
+    const exporter = target == "es6" ? "" : "module.exports = ";
+    const output = target == "es6" ? "es6-module" : "source";
+    return filter(id) ?
+      {
+        code: `${exporter}${buildParser(grammar, { output })};`,
+        map: { mappings: "" }
+      } :
+      null;
   }
 })
